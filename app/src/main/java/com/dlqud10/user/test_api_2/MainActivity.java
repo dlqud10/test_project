26baf50ca69d97a1a93ee01bc73832fd5c9fa647
dlqud10 <dlqud10@naver.com>
2018-05-03 16:25:03 +0900

    change some UI

--- app/src/main/java/com/dlqud10/user/test_api_2/MainActivity.java
+++ app/src/main/java/com/dlqud10/user/test_api_2/MainActivity.java
@@ -1,7 +1,12 @@
 package com.dlqud10.user.test_api_2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
+
 }
