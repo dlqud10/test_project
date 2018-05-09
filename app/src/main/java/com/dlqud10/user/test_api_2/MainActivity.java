4cc84e09dd36a117593f0e3ad481b9b197f5cfd0
dlqud10 <dlqud10@naver.com>
2018-05-09 14:31:30 +0900

    v0.3 succeed in make directory , text and wirte on text

--- app/src/main/java/com/dlqud10/user/test_api_2/MainActivity.java
+++ app/src/main/java/com/dlqud10/user/test_api_2/MainActivity.java
@@ -2,21 +2,25 @@
  * @file MainActivity.java
  * @brief The UI of using exchange rate page
  * @author 이병현
- * @date 2018.05.03
- * @version ERP v0.1
- * @state The function of parsing json text in URL is unsuccessful
+ * @date 2018.05.09
+ * @version ERP v0.2
+ * @state The function of parsing json text in URL is unsuccessful , make text file had success
  */
 
 package com.dlqud10.user.test_api_2;
 
-import android.app.Activity;
+import java.io.*;
+
+
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 import com.google.gson.JsonElement;
@@ -28,31 +32,53 @@ import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+
+        final static String foldername = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Test_Api";
+        final static String filename = "testN!.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.addListButton);
-        btn.setOnClickListener(new Button.OnClickListener() {
 
+
+
+        Log.d("path",foldername);
+
+        Button btnMakeText = (Button) findViewById(R.id.makeTextButton);
+        btnMakeText.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mOnFileWrite(v);
+            }
+
+        });
+
+        Button btnAddList = (Button) findViewById(R.id.addListButton);
+        btnAddList.setOnClickListener(new Button.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                EditText et = (EditText) findViewById(R.id.editText);
+                EditText et = (EditText) findViewById(R.id.editTextConfirm);
 
                 URL url = null;
                 HttpURLConnection urlConnection = null;
                 BufferedInputStream buf = null;
 
-                et.append("ok");
+                et.append("ok2");
 
                 try {
 
@@ -82,19 +108,81 @@ public class MainActivity extends AppCompatActivity {
                     JsonObject curl_nm = json.getAsJsonObject("curl_nm");
                     System.out.println("curl_unit:" + curl_unit + "curl_nm:" + curl_nm);
 
-                    // et.append(curl_unit+"\n");
-                    // et.append(curl_nm+"\n");
+                    et.append(curl_unit + "\n");
+                    et.append(curl_nm + "\n");
 
 
                 } catch (Exception e) {
-                    //et.setText(e.getMessage());
-                } finally {
-                    urlConnection.disconnect();
+                    e.printStackTrace();
                 }
-
             }
         });
-
-
     }
+
+    //버튼클릭
+    public void mOnFileWrite(View v){
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String contents = "Log 생성 : "+now+"\n";
+
+        WriteTextFile(foldername, filename, contents);
+    }
+
+    //텍스트내용을 경로의 텍스트 파일에 쓰기
+    public void WriteTextFile(String foldername, String filename, String contents){
+        try{
+            File dir = new File (foldername);
+            //디렉토리 폴더가 없으면 생성함
+            if(!dir.exists()){
+                dir.mkdir();
+            }
+
+            dir.createNewFile();
+
+            Toast.makeText(this, "파일생성 성공~", Toast.LENGTH_SHORT).show();
+
+            //파일 output stream 생성
+            FileOutputStream fos = new FileOutputStream(foldername+"/"+filename, true);
+            //파일쓰기
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos));
+            writer.write(contents);
+            writer.flush();
+
+            writer.close();
+            fos.close();
+        }catch (IOException e){
+            Toast.makeText(this, "파일생성 실패~", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    /*
+    private void textFile()
+    {
+
+        String dirPath = getFilesDir().getAbsolutePath();
+        File file = new File(dirPath);
+
+        //일치하는 폴더가 없으면 생성
+        if (!file.exists() )
+        {
+            file.mkdirs();
+            Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+        }
+
+        String testStr = "complete";
+        File savefile = new File(dirPath+"/test.txt");
+        try{
+            FileOutputStream fos = new FileOutputStream(savefile);
+            fos.write(testStr.getBytes());
+            fos.close();
+            Toast.makeText(this, " save Success", Toast.LENGTH_SHORT).show();;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    */
+
+
 }
